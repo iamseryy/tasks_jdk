7e1a485438690477bb3179598f9e5330243c82c7
desktop <psomail@yandex.ru>
2023-12-27 22:58:32 +0300

    task5 ready simple version_ minor fix

--- task5/Five_philosophers/src/main/java/ru/task5/model/DinnerTable.java
+++ task5/Five_philosophers/src/main/java/ru/task5/model/DinnerTable.java
@@ -6,13 +6,13 @@ public class DinnerTable {
     private final String DINNER_TABLE_RESERVED = "The dinning table is reserved by %s.\n";
     private final String  PHILOSOPHER_IS_EATING = "%s is eating.\n";
     private final String  PHILOSOPHER_HAS_EATEN = "%s has just eaten. The dinning table is free.\n";
-    private static final int DURATION_EATING_ACTIVITY_IN_MILLISECONDS = 500;
+    private static final int DURATION_RESERVED_IN_MILLISECONDS = 500;
 
     public synchronized void reserved() {
         try {
             System.out.printf(DINNER_TABLE_RESERVED, Thread.currentThread().getName());
             System.out.printf(PHILOSOPHER_IS_EATING, Thread.currentThread().getName());
-            TimeUnit.MILLISECONDS.sleep(DURATION_EATING_ACTIVITY_IN_MILLISECONDS);
+            TimeUnit.MILLISECONDS.sleep(DURATION_RESERVED_IN_MILLISECONDS);
             System.out.printf(PHILOSOPHER_HAS_EATEN, Thread.currentThread().getName());
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();

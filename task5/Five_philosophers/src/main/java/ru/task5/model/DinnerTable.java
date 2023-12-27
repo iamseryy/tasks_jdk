733a37e33044a46b9eda9f5e9aea0f6508b94854
desktop <psomail@yandex.ru>
2023-12-27 23:53:32 +0300

    task5 start full version

--- task5/Five_philosophers/src/main/java/ru/task5/model/DinnerTable.java
+++ task5/Five_philosophers/src/main/java/ru/task5/model/DinnerTable.java
@@ -8,12 +8,15 @@ public class DinnerTable {
     private final String  PHILOSOPHER_HAS_EATEN = "%s has just eaten. The dinning table is free.\n";
     private static final int DURATION_RESERVED_IN_MILLISECONDS = 500;
 
+    private static String pre
+
     public synchronized void reserved() {
         try {
             System.out.printf(DINNER_TABLE_RESERVED, Thread.currentThread().getName());
             System.out.printf(PHILOSOPHER_IS_EATING, Thread.currentThread().getName());
             TimeUnit.MILLISECONDS.sleep(DURATION_RESERVED_IN_MILLISECONDS);
             System.out.printf(PHILOSOPHER_HAS_EATEN, Thread.currentThread().getName());
+            notifyAll();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
--- task5/Five_philosophers/src/main/java/ru/task5/model/Philosopher.java
+++ task5/Five_philosophers/src/main/java/ru/task5/model/Philosopher.java
@@ -1,16 +1,24 @@
 package ru.task5.model;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class Philosopher implements Runnable{
     private static final String THINK_ACTIVITY = "%s is thinking now.\n";
+    private static final String PHILOSOPHER_WANT_EAT = "%s want to eat.\n";
     private static final int LIFE_DURATION = 3;
+    private static final int MAX_DURATION_THOUGHT = 1000;
+    private static final int MIN_DURATION_THOUGHT = 1000;
+    private final Random randThoughtProcess;
+
     private String name;
     private DinnerTable dinnerTable;
 
     public Philosopher(String name, DinnerTable dinnerTable) {
         this.name = name;
         this.dinnerTable = dinnerTable;
+        this.randThoughtProcess = new Random();
     }
 
     public String getName() {
@@ -18,7 +26,34 @@ public class Philosopher implements Runnable{
     }
 
     private void live() {
-        System.out.printf(THINK_ACTIVITY, this.name);
+        think();
+        eat();
+        synchronized (this) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+    }
+
+    private void think() {
+        try {
+            System.out.printf(THINK_ACTIVITY, this.name);
+
+            if(!name.equals("Philosopher3")) {
+            TimeUnit.MILLISECONDS.sleep(randThoughtProcess.nextInt((MAX_DURATION_THOUGHT - MIN_DURATION_THOUGHT) + 1) +
+                    MIN_DURATION_THOUGHT);
+            }
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void eat() {
+        System.out.printf(PHILOSOPHER_WANT_EAT, this.name);
         dinnerTable.reserved();
     }
 

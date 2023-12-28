d9d407d2cb6c459f89b8182aff29025b57e64730
Сергей Олегович <s_perminov@bz.ru>
2023-12-28 13:53:55 +0300

    task5 ready

--- task5/Five_philosophers/src/main/java/ru/task5/model/DinnerTable.java
+++ task5/Five_philosophers/src/main/java/ru/task5/model/DinnerTable.java
@@ -1,24 +1,48 @@
 package ru.task5.model;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DinnerTable {
-    private final String DINNER_TABLE_RESERVED = "The dinning table is reserved by %s.\n";
-    private final String  PHILOSOPHER_IS_EATING = "%s is eating.\n";
-    private final String  PHILOSOPHER_HAS_EATEN = "%s has just eaten. The dinning table is free.\n";
-    private static final int DURATION_RESERVED_IN_MILLISECONDS = 500;
+    private static final String DINNER_TABLE_RESERVED = "The dinning table is reserved by %s.\n";
+    private static final String PHILOSOPHER_IS_EATING = "%s is eating.\n";
+    private static final String PHILOSOPHER_HAS_EATEN = "%s has just eaten. The dinning table is free.\n";
+    private static final String WAIT_TURN = "Although %s is hungry, he will have to wait.\n";
+    private static final int DURATION_RESERVED_IN_MS = 500;
+    private static final int TURN_WAITING_TIME_IN_MS = 15000;
+    private final Lock lock;
+    private final Condition condition;
+    private static Philosopher previousPhilosopher;
 
-    private static String pre
+    public DinnerTable() {
+        this.lock = new ReentrantLock();
+        this.condition = this.lock.newCondition();
+    }
 
-    public synchronized void reserved() {
+    public void reserved(Philosopher philosopher) {
+        this.lock.lock();
         try {
+            while (previousPhilosopher != null && previousPhilosopher.equals(philosopher)) {
+                System.out.printf(WAIT_TURN, philosopher.getName());
+                this.condition.await(TURN_WAITING_TIME_IN_MS, TimeUnit.MILLISECONDS);
+            }
+
             System.out.printf(DINNER_TABLE_RESERVED, Thread.currentThread().getName());
             System.out.printf(PHILOSOPHER_IS_EATING, Thread.currentThread().getName());
-            TimeUnit.MILLISECONDS.sleep(DURATION_RESERVED_IN_MILLISECONDS);
+
+            TimeUnit.MILLISECONDS.sleep(DURATION_RESERVED_IN_MS);
+
             System.out.printf(PHILOSOPHER_HAS_EATEN, Thread.currentThread().getName());
-            notifyAll();
+
+            previousPhilosopher = philosopher;
+
+            this.condition.signalAll();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+        } finally {
+            this.lock.unlock();
         }
     }
 }
--- task5/Five_philosophers/src/main/java/ru/task5/model/Philosopher.java
+++ task5/Five_philosophers/src/main/java/ru/task5/model/Philosopher.java
@@ -1,5 +1,6 @@
 package ru.task5.model;
 
+import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
@@ -8,8 +9,8 @@ public class Philosopher implements Runnable{
     private static final String THINK_ACTIVITY = "%s is thinking now.\n";
     private static final String PHILOSOPHER_WANT_EAT = "%s want to eat.\n";
     private static final int LIFE_DURATION = 3;
-    private static final int MAX_DURATION_THOUGHT = 1000;
-    private static final int MIN_DURATION_THOUGHT = 1000;
+    private static final int MAX_DURATION_THOUGHT_MS = 10000;
+    private static final int MIN_DURATION_THOUGHT_MS = 1;
     private final Random randThoughtProcess;
 
     private String name;
@@ -28,25 +29,13 @@ public class Philosopher implements Runnable{
     private void live() {
         think();
         eat();
-        synchronized (this) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-
     }
 
     private void think() {
         try {
             System.out.printf(THINK_ACTIVITY, this.name);
-
-            if(!name.equals("Philosopher3")) {
-            TimeUnit.MILLISECONDS.sleep(randThoughtProcess.nextInt((MAX_DURATION_THOUGHT - MIN_DURATION_THOUGHT) + 1) +
-                    MIN_DURATION_THOUGHT);
-            }
-
+            TimeUnit.MILLISECONDS.sleep(randThoughtProcess.nextInt((MAX_DURATION_THOUGHT_MS - MIN_DURATION_THOUGHT_MS) + 1) +
+                    MIN_DURATION_THOUGHT_MS);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
@@ -54,11 +43,24 @@ public class Philosopher implements Runnable{
 
     private void eat() {
         System.out.printf(PHILOSOPHER_WANT_EAT, this.name);
-        dinnerTable.reserved();
+        dinnerTable.reserved(this);
     }
 
     @Override
     public void run() {
         IntStream.range(0,LIFE_DURATION).forEach(it -> live());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Philosopher philosopher = (Philosopher) o;
+        return Objects.equals(this.name, philosopher.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name);
     }
 }
